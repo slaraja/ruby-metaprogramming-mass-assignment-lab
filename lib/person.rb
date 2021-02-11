a7bb2de635c1b25b684f2a96158d08871630f402
Stephanie La Raja <stephanielaraja@gmail.com>
2021-02-11 15:36:52 -0500

    commit tree b6ed0b4e19a4aaf42082522ab583314b17b8d4a9

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
+
 class Person
-  #your code here
-end
+  def initialize(attributes)
+
+    attributes.each do |key, value| 
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+
+      end
+  end
+
+end 
